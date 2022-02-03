d9b30de9c940dac2d87398d2996acb4400911f46
unknown <calvinp003@gmail.com>
2022-02-02 17:32:37 -0800

    change puppeteer args

--- scraper.js
+++ scraper.js
@@ -17,6 +17,7 @@ const chromium = require("chrome-aws-lambda");
 module.exports.getMediaSources = function (pageUrl, options = {}) {
     return new Promise(async (resolve) => {
         let browser = await chromium.puppeteer.launch({
+            args: ["--no-sandbox", "--disable-setuid-sandbox"],
             executablePath: await chromium.executablePath,
         });
         let page = await browser.newPage();
